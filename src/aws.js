5af68881b4fb56875416bce89eadb54de7f99dca
HendriceH <110101174+HendriceH@users.noreply.github.com>
2023-11-14 09:19:20 +0100

    Update aws.js

--- src/aws.js
+++ src/aws.js
@@ -48,7 +48,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
     BlockDeviceMappings: [{
-        DeviceName: '/dev/nvme0n1',
+        DeviceName: '/dev/nvme',
         Ebs: {
           DeleteOnTermination: true,
           VolumeSize: 30,
